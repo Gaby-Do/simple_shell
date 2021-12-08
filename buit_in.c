6e4ac3212eb7857791de90dcb4639e7a4db979b5
Juan Gimenez <juancitogimenez0@gmail.com>
2021-12-07 18:24:00 -0800

    left everything as it was

--- buit_in.c
+++ buit_in.c
@@ -3,17 +3,17 @@
  *_env - prints current env
  *Return: 0 succes
  */
-void _env(void)
+int _env(void)
 {
   int index = 0;
 
-  while (environ[index] != NULL)
+  while (environ[index])
     {
-      write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-      write(STDOUT_FILENO, "\n", 1); /** puts not worked*/
+      _prints(environ[index]);
+      _putchar('\n');
+      ++index;
     }
-}
-     
+  return (0);
 }
 
 /**
--- shell.h
+++ shell.h
@@ -16,7 +16,7 @@ extern char **environ;
 
 /* builtins*/
 void built_in(char *string);
-void _env(void);
+int _env(void);
 int ex_it(void);
 
 /* de impresión*/
